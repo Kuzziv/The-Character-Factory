499e7f024577451662efca4cca5552ae111a525e
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-02-14 13:18:03 +0100

    Update to Poo

--- TheCharacterFactory/MockData/MockCharacter.cs
+++ TheCharacterFactory/MockData/MockCharacter.cs
@@ -7,8 +7,8 @@ namespace TheCharacterFactory.MockData
     {
         public static List<Character> _character = new List<Character>()
         {
-            new Character("Foo","The Holy Light","I'm the shit","Foo Fun", 61675837),
-            new Character("Poo","The Dark Side","I'm your farther","Poo Fun", 61675837),
+            new Character("Foo","The Holy Light","I'm your father","Foo Fun", 61675837),
+            new Character("Poo","The Dark Side","I'm the shit","Poo Fun", 61675837),
             new Character("Bar","","I'm your farther","Bar Fun", 61675837),
             new Character("Sky","","light","Sky Fun", 61675837)
         };
