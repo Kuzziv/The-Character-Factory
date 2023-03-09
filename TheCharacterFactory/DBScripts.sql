c300716d349a29b3f4d2082ea7b80bd29c9fb5b0
Canty <chri38eb@edu.zealand.dk>
2023-03-09 22:45:47 +0100

    DB Update

--- TheCharacterFactory/DBScripts.sql
+++ TheCharacterFactory/DBScripts.sql
@@ -3,8 +3,19 @@
 
 
 
-
-
+CREATE TABLE [dbo].[Power] (
+    [Id]             INT            NOT NULL,
+    [Name]           VARBINARY (50) NOT NULL,
+    [Description]    VARCHAR (1000) NOT NULL,
+    [Type]           VARCHAR (50)   NOT NULL,
+    [BlockedPowers]  BINARY (50)    NULL,
+    [BlocksMagic]    BINARY (50)    NOT NULL,
+    [RequiredPower1] INT            NULL,
+    [RequiredPower2] INT            NULL,
+    [RequiredPower3] INT            NULL,
+    [RequiredPower4] INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
 
 CREATE TABLE [dbo].[Character] (
     [CharacterID]   INT            NOT NULL,
@@ -21,3 +32,23 @@ CREATE TABLE [dbo].[CharacterPower] (
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
+CREATE TABLE [dbo].[Magic] (
+    [Id]             INT            NOT NULL,
+    [Name]           VARCHAR (50)   NOT NULL,
+    [Description]    VARCHAR (1000) NOT NULL,
+    [Type]           VARCHAR (50)   NOT NULL,
+    [RequiredEvne1]  INT            NULL,
+    [RequiredEvne2]  INT            NULL,
+    [RequiredEvne3]  INT            NULL,
+    [RequiredMagic1] INT            NULL,
+    [RequiredMagic2] INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[PowerMagic] (
+    [Id]       INT NOT NULL,
+    [PowerId]  INT NULL,
+    [MagicId1] INT NULL,
+    [MagicId2] INT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
