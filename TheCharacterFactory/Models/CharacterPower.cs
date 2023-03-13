cc9b5788dac72b98712c1eb0df98dbd6d927b2dd
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-13 12:43:07 +0100

    Class update

---
+++ TheCharacterFactory/Models/CharacterPower.cs
@@ -0,0 +1,21 @@
+﻿namespace TheCharacterFactory.Models
+{
+    public class CharacterPower
+    {
+        public int id { get; set; }
+        public int CharacterId { get; set; }
+        public int PowerMagicId { get; set; }
+
+        public CharacterPower()
+        {
+            
+        }
+
+        public CharacterPower(int id, int characterId, int powerMagicId)
+        {
+            this.id = id;
+            CharacterId = characterId;
+            PowerMagicId = powerMagicId;
+        }
+    }
+}
