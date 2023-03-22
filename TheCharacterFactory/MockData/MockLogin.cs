ad80d8716157447887fe1a545339ddbfd0c1dc49
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-22 11:24:09 +0100

    bug fix

--- TheCharacterFactory/MockData/MockLogin.cs
+++ TheCharacterFactory/MockData/MockLogin.cs
@@ -1,6 +1,20 @@
-﻿namespace TheCharacterFactory.MockData
+﻿using TheCharacterFactory.Models;
+
+namespace TheCharacterFactory.MockData
 {
     public class MockLogin
     {
+        public static List<Login> logins = new List<Login>()
+        {
+            new Login("Geja", "1234", false, "Mads Egelund Ludvigsen", "61675837"),
+            new Login("CC", "1234", true, "Christian Canty", "51528341"),
+            new Login("Nugget", "1234", false, "Philip Mark Nielsen", "42216198"),
+            new Login("Taz", "1234", true, "Mille Nikita Alstrøm", "21523132")
+        };
+
+        public static List<Login> GetAllLogins()
+        {
+            return logins;
+        }
     }
 }
