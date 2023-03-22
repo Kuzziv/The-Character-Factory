ae214dbaa4dddb582df39232d91448bc01eb8a29
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-22 10:41:47 +0100

    power's props can now be null

--- TheCharacterFactory/MockData/MockPower.cs
+++ TheCharacterFactory/MockData/MockPower.cs
@@ -9,7 +9,9 @@ namespace TheCharacterFactory.MockData
             new Power("Super stræk", "du kan løfte double så meget som normal","Evne", false, 1, 2, 3, 4, 5),
             new Power("Lestig", "du bevæger dig mere lydløst", "Evne", false, 1, 2, 3, 4, 5),
             new Power("Ild skyd", "Du kan kaste med ild", "Magi", false, 1, 2, 3, 4, 5),
-            new Power("Frost", "Du kan fryse din modstander fast", "Magi", true, 1, 2, 3, 4, 5)
+            new Power("Frost", "Du kan fryse din modstander fast", "Magi", true, 1, 2, 3, 4, 5),
+            new Power("ISS skud", "Isbold der kommer fylven ud fra din hånd", "Magi",true, null, null, null, null, null)
+            
         };
 
         public static List<Power> GetAllPowers()
--- TheCharacterFactory/Models/Power.cs
+++ TheCharacterFactory/Models/Power.cs
@@ -9,17 +9,17 @@
         public string Description { get; set; }
         public string Type { get; set; }
         public bool BlocksMagic { get; set; }
-        public int BlockedEvne { get; set; }
-        public int RequiredEvne1 { get; set; }
-        public int RequiredEvne2 { get; set; }
-        public int RequiredEvne3 { get; set; }
-        public int RequiredEvne4 { get; set; }
+        public int? BlockedEvne { get; set; }
+        public int? RequiredEvne1 { get; set; }
+        public int? RequiredEvne2 { get; set; }
+        public int? RequiredEvne3 { get; set; }
+        public int? RequiredEvne4 { get; set; }
         
         public Power()
         {            
         }
 
-        public Power(string name, string description, string type, bool blocksMagic, int blockedEvne, int requiredEvne1, int requiredEvne2, int requiredEvne3, int requiredEvne4)
+        public Power(string name, string description, string type, bool blocksMagic, int? blockedEvne, int? requiredEvne1, int? requiredEvne2, int? requiredEvne3, int? requiredEvne4)
         {
             Id = nextid++;
             Name = name;
