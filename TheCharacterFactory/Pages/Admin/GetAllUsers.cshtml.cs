bd7f8ff9482de5f3884eea76583e3c0a090069b0
Canty <chri38eb@edu.zealand.dk>
2023-03-22 10:53:19 +0100

    Update to CHaracter class and added admin file in pages

---
+++ TheCharacterFactory/Pages/Admin/GetAllUsers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheCharacterFactory.Pages.Admin
+{
+    public class GetAllUsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
